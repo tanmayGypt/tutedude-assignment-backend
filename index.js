88002b99084839c806a6589820863afe7984e7df
tanmaygolokait <tanmay@golokait.com>
2024-09-16 01:40:11 +0530

    all

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://66e73c83481c844594e63773--taupe-cactus-fb9d41.netlify.app",
+    origin: "https://66e73edb422d0d0008dcac92--taupe-cactus-fb9d41.netlify.app",
     credentials: true,
   })
 );
