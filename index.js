ee96b1d4c746fc82720dab4f7020207695c3b08b
tanmaygolokait <tanmay@golokait.com>
2024-09-16 01:51:21 +0530

    all

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use("/api/friends", auth, friendRoutes);
+app.use("/api/friends", friendRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
